d3192d8007dcbfb3993e772c3bfbfa7a2d9feca5
PokemonBros <pokebros@users.noreply.github.com>
2019-06-10 22:51:12 -0400

    First update

--- Shu Bot/modules/game.py
+++ Shu Bot/modules/game.py
@@ -2,6 +2,7 @@
 
 #Imports
 import discord  
+import random
 from discord.ext import commands
 
 class Game(commands.Cog):
@@ -13,6 +14,35 @@ class Game(commands.Cog):
         await ctx.send('You have been invited to play russian roulette ' + userid2 + "." + " Do you accept? Y/N")
         
         await ctx.send('Welcome to russian roulette.')
+    @commands.command()
+    async def rps(self, ctx, user_input: str): 
+        cpu_rps = ["rock", "paper", "scissors"]
+        cpu_choice = random.choice(cpu_rps)
+        user_choice = user_input.lower()
+        if user_choice == "rock":
+            if cpu_choice == "rock":
+                await ctx.send("You have tied with a bot. Shows you why you\'re being replaced.")
+            if cpu_choice == "paper":
+                await ctx.send("You won. Congratulations, it's a celebration, hopefully this doesn\'t earn me a demonitization.")
+            if cpu_choice == "scissors":
+                await ctx.send("Yikes. You feeling like pennies?")
+        if user_choice == "paper":
+            if cpu_choice == "rock":
+                await ctx.send("Yikes. You feeling like pennies?")
+            if cpu_choice == "paper":
+                await ctx.send("You have tied with a bot. Shows you why you\'re being replaced.")
+            if cpu_choice == "scissors":
+                await ctx.send("You won. Congratulations, it's a celebration, hopefully this doesn\'t earn me a demonitization.")
+        if user_choice == "scissors":
+            if cpu_choice == "rock":
+                await ctx.send("Yikes. You feeling like pennies?")
+            if cpu_choice == "paper":
+                await ctx.send("You won. Congratulations, it's a celebration, hopefully this doesn\'t earn me a demonitization.")
+            if cpu_choice == "scissors":
+                await ctx.send("You have tied with a bot. Shows you why you\'re being replaced.")
+
+        
+
 
 
 def setup(bot):
--- Shu Bot/modules/math.py
+++ Shu Bot/modules/math.py
@@ -9,16 +9,28 @@ class Game(commands.Cog):
         self.bot = bot
     @commands.command()
     async def rroulette(self, ctx, *, equation : str): 
+        equation_numbers = []
+        equation_signs = []
         check = True
         temp = ""
+        counter = 0
         equation.replace(" ", "")
         while check:
             for letter in equation:
                 if letter.isdigit():
                     temp = "" + letter
-                else
-                    break
+                    counter++
                 
+                elif letter == "^":
+                    temp = "" + letter
+                else
+                    temp =
+                    break
+            equation_numbers.append(temp)
+            temp = ""
+
+
+
 
 def setup(bot):
     bot.add_cog(Game(bot))
